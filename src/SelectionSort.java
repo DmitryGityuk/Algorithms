b8c2a2ec8966bed210d2e2290e14a6ca4b298d97
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-04-19 21:36:53 +0500

    added SelectionSort algorithm

---
+++ src/SelectionSort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+/**
+ * Принцип работы:
+ * 1. Выполняется проход по сортируемой последовательности.
+ * 2. Первый элемент не отсортированной подпоследовательности принимается за минимальный и сохраняется его индекс.
+ * 3. Выполняется проход по не отсортирвоанной части последовательности в поиске минимального элемента.
+ * 4. Происходит обмен найденного манимального члена и первого члена не отсортированной подпоследовательности.
+ */
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{5, 0, -2, 10, 22, 8, 1, 4, 99};
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
