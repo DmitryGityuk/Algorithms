8f3d234bc4283d5f439cd281ea44660f44c778e6
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-04-19 21:36:28 +0500

    added BubbleSort algorithm

---
+++ src/BubbleSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void sort(int[] arr) {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i - 1] > arr[i]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[i - 1];
+                    arr[i - 1] = tmp;
+                    isSorted = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{5, 0, -2, 10, 1, 8};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
