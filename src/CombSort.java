ca3121b2983038f39cc3f32ff5b61b8d9af1d45c
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-04-21 23:09:28 +0500

    added CombSort

---
+++ src/CombSort.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class CombSort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{43, 56, 72, 90, 14, -23, 19, 27, 44, 1};
+        combSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void combSort(int[] arr) {
+        int gap = arr.length;
+        boolean isSorted = false;
+        while (!isSorted || gap != 1) {
+            if (gap > 1) {
+                gap = gap * 10 / 13;
+            } else {
+                gap = 1;
+            }
+            isSorted = true;
+            for (int i = gap; i < arr.length; i++) {
+                if (arr[i - gap] > arr[i]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[i - gap];
+                    arr[i - gap] = tmp;
+                    isSorted = false;
+                }
+
+            }
+        }
+    }
+}
