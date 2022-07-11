7209f97235087551b116cfc63ff9f6bf254e0c77
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-07-11 15:57:11 +0500

    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice.

---
+++ src/tasks/LeetCode/TwoSum.java
@@ -0,0 +1,34 @@
+package tasks.LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No solution for defined input data");
+    }
+
+    class SolutionTwo {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                map.put(nums[i], i);
+            }
+            for (int i = 0; i < nums.length; i++) {
+                int requiredNUm = target - nums[i];
+
+                if (map.containsKey(requiredNUm) && map.get(requiredNUm) != i) {
+                    return new int[]{i, map.get(requiredNUm)};
+                }
+            }
+            throw new IllegalArgumentException("No solution for defined input data");
+        }
+    }
+}
