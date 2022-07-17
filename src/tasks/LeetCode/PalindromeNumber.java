e97a5afa93fbc6acb218e5bb6e3f47c225bbcc76
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-07-17 23:09:32 +0500

    Given an integer x, return true if x is palindrome integer. An integer is a palindrome when it reads the same backward as forward.

---
+++ src/tasks/LeetCode/PalindromeNumber.java
@@ -0,0 +1,24 @@
+package tasks.LeetCode;
+
+/**
+ * 9.
+ * https://leetcode.com/problems/palindrome-number/
+ */
+
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        if (x % 10 == 0 && x != 0) {
+            return false;
+        }
+        int invertedNumber = 0;
+        int copyOfX = x;
+        while (copyOfX != 0) {
+            invertedNumber = invertedNumber * 10 + copyOfX % 10;
+            copyOfX = copyOfX / 10;
+        }
+        return invertedNumber == x;
+    }
+}
