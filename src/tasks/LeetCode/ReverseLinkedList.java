06886513d6f99e4f4a19f57036ffc6c5838795e9
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-09-13 23:38:34 +0500

    Given the head of a singly linked list, reverse the list, and return the reversed list.

---
+++ src/tasks/LeetCode/ReverseLinkedList.java
@@ -0,0 +1,30 @@
+package tasks.LeetCode;
+
+/**
+ * 206. Reverse Linked List
+ * https://leetcode.com/problems/reverse-linked-list/
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode previousNode = null;
+        ListNode currentNode = head;
+        while (currentNode != null){
+            ListNode nextNode = currentNode.next;
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+            currentNode = nextNode;
+        }
+        return previousNode;
+    }
+}
