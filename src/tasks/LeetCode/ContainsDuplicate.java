c5cc9564da16feb11611dc5cad16bcf0a465137f
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-09-16 00:18:14 +0500

    Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.

---
+++ src/tasks/LeetCode/ContainsDuplicate.java
@@ -0,0 +1,32 @@
+package tasks.LeetCode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 217. Contains Duplicate
+ * https://leetcode.com/problems/contains-duplicate/
+ */
+
+public class ContainsDuplicate {
+    public boolean containsDuplicateV1(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsDuplicateV2(int[] nums) {
+        Set<Integer> integers = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!integers.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
