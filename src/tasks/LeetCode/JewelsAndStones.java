b996eb0b6158479a65f94b3dda4b1697da5013b1
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-09-16 21:34:39 +0500

    You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
    Letters are case sensitive, so "a" is considered a different type of stone from "A".

---
+++ src/tasks/LeetCode/JewelsAndStones.java
@@ -0,0 +1,24 @@
+package tasks.LeetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 771. Jewels and Stones
+ * https://leetcode.com/problems/jewels-and-stones/
+ */
+public class JewelsAndStones {
+    public int numJewelsInStones(String jewels, String stones) {
+        int result = 0;
+        Set<Character> chars = new HashSet<>();
+        for (Character c : jewels.toCharArray()) {
+            chars.add(c);
+        }
+        for (Character c : stones.toCharArray()) {
+            if (chars.contains(c)) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
