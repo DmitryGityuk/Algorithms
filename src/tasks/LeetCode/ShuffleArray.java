673993263979c0c300d4a9846e58b49bf2d34036
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-09-16 21:21:58 +0500

    Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn]. Return the array in the form [x1,y1,x2,y2,...,xn,yn].

---
+++ src/tasks/LeetCode/ShuffleArray.java
@@ -0,0 +1,18 @@
+package tasks.LeetCode;
+
+/**
+ * 1470. Shuffle the Array
+ * https://leetcode.com/problems/shuffle-the-array/
+ */
+public class ShuffleArray {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[nums.length];
+        int left = 0;
+        int right = n;
+        for (int i = 1; i < nums.length; i += 2) {
+            result[i - 1] = nums[left++];
+            result[i] = nums[right++];
+        }
+        return result;
+    }
+}
