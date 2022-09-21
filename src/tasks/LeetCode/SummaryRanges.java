0b732edbba7c88143fee0e1aa080d2013acd32a4
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-09-22 00:21:06 +0500

    You are given a sorted unique integer array nums. A range [a,b] is the set of all integers from a to b (inclusive). Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.

---
+++ src/tasks/LeetCode/SummaryRanges.java
@@ -0,0 +1,26 @@
+package tasks.LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 228. Summary Ranges
+ * https://leetcode.com/problems/summary-ranges/
+ */
+public class SummaryRanges {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int start = nums[i];
+            while (i + 1 < nums.length && nums[i] + 1 == nums[i + 1]) {
+                i++;
+            }
+            if (start != nums[i]) {
+                result.add(start + "->" + nums[i]);
+            } else {
+                result.add(start + "");
+            }
+        }
+        return result;
+    }
+}
