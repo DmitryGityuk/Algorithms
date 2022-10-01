40cf2c8b4a21a571fd8c794fe09a15ee99bfffa8
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-10-02 00:39:45 +0500

    A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers. Given a string s, return true if it is a palindrome, or false otherwise.

---
+++ src/tasks/LeetCode/ValidPalindrome.java
@@ -0,0 +1,29 @@
+package tasks.LeetCode;
+
+/**
+ * 125. Valid Palindrome
+ * https://leetcode.com/problems/valid-palindrome/submissions/
+ */
+
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        if (s == null) {
+            throw new IllegalArgumentException("string cannot be null");
+        }
+        if (s.isEmpty()) {
+            return true;
+        }
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.toLowerCase();
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
