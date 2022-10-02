20058cf80f2dd41f474271969d0a11890ef6fd0d
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-10-02 23:36:35 +0500

    Given an array nums of size n, return the majority element. The majority element is the element that appears more than [n / 2] times. You may assume that the majority element always exists in the array.

---
+++ src/tasks/LeetCode/MajorityElement.java
@@ -0,0 +1,23 @@
+package tasks.LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 169. Majority Element
+ * https://leetcode.com/problems/majority-element/
+ */
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            if (map.get(num) > nums.length / 2) {
+                return num;
+            }
+        }
+        return 0;
+    }
+}
