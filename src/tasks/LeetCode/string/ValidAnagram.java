e6e9abc8bba7b0935241bb7adc0ee75b24df91f7
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-10-10 23:34:39 +0500

    Added solution

--- src/tasks/LeetCode/string/ValidAnagram.java
+++ src/tasks/LeetCode/string/ValidAnagram.java
@@ -1,5 +1,8 @@
 package tasks.LeetCode.string;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 242. Valid Anagram
  * https://leetcode.com/problems/valid-anagram/
@@ -13,9 +16,26 @@ public class ValidAnagram {
             counter[s.charAt(i) - 'a']++;
             counter[t.charAt(i) - 'a']--;
         }
-        for (int count : counter){
+        for (int count : counter) {
             if (count != 0) return false;
         }
         return true;
     }
+
+    class Solution {
+        public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) return false;
+            Map<Character, Integer> s1 = new HashMap<>();
+            Map<Character, Integer> s2 = new HashMap<>();
+            for (int i = 0; i < s.length(); i++) {
+                s1.put(s.charAt(i), s1.getOrDefault(s.charAt(i), 0) + 1);
+                s2.put(t.charAt(i), s2.getOrDefault(t.charAt(i), 0) + 1);
+            }
+            for (char c : s1.keySet()) {
+                if (!s2.containsKey(c) || !s1.get(c).equals(s2.get(c))) return false;
+            }
+            return true;
+        }
+    }
+
 }
