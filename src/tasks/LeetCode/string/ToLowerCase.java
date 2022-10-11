6d57fe54eb7eed898f1b552c5b57675ac585a236
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-10-11 23:26:58 +0500

    Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.

---
+++ src/tasks/LeetCode/string/ToLowerCase.java
@@ -0,0 +1,11 @@
+package tasks.LeetCode.string;
+
+/**
+ * 709. To Lower Case
+ * https://leetcode.com/problems/to-lower-case/
+ */
+public class ToLowerCase {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
